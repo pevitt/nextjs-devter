4770dfebdb3ea6337d2cbac3ba4dd02b6f1d4061
pevitt <jrigoberto17@gmail.com>
2021-02-17 18:33:53 -0500

    video2

--- pages/index.js
+++ pages/index.js
@@ -15,24 +15,8 @@ export default function Home() {
       <h1 className={styles.title}>
           <a href="https://nextjs.org">Devter</a>
         </h1>
-        <nav>
-          <Link className={styles.orange} href='/timeline'>
-            Timeline
-          </Link>
-        </nav>
         </main>
        
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
