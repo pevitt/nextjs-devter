386b072d90649feb10fa27847f7e4c4d4c8f3b2c
pevitt <jrigoberto17@gmail.com>
2021-02-21 17:48:23 -0500

    video 2

---
+++ firebase/client.js
@@ -0,0 +1,53 @@
+import firebase from 'firebase/app'
+import "firebase/auth"
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCFdKvyrokTu1T6RSspnvtzmBJ8Vrrsy_U",
+    authDomain: "devter-7ed49.firebaseapp.com",
+    projectId: "devter-7ed49",
+    storageBucket: "devter-7ed49.appspot.com",
+    messagingSenderId: "99053753565",
+    appId: "1:99053753565:web:1f70d30d576a3f81247734",
+    measurementId: "G-VB2NXVS35M"
+  };
+
+  !firebase.apps.length &&
+  firebase.initializeApp(firebaseConfig)
+
+  export const auth = firebase.auth() 
+
+const mapUserFromFirebaseAuthToUser = (user) => {
+  const {displayName, email, photoURL } = user
+
+  return {
+    avatar: photoURL,
+    username: displayName,
+    email
+  }
+}
+
+export const onAuthStateChanged = (onChange) => {
+  return firebase
+    .auth()
+    .onAuthStateChanged(user => {
+        if (user){
+            console.log(user);
+            const normalizedUser = mapUserFromFirebaseAuthToUser(user)
+            onChange(normalizedUser)
+        }
+        
+    })
+}
+
+export const loginWithGitHub = () => {
+  const githubProvider = new firebase.auth.GithubAuthProvider()
+    
+  return firebase
+    .auth()
+    .signInWithPopup(githubProvider)
+    // console.log(user_result);
+    
+}
+
+  
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
+import {useEffect, useState} from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import AppLayout from '../components/AppLayout'
+import Button from '../components/Button'
+import GitHub from '../components/Icons/Github'
 import styles from '../styles/Home.module.css'
 
+import {
+  loginWithGitHub,
+  onAuthStateChanged
+} from '../firebase/client'
+
 export default function Home() {
+  const [user, setUser] = useState(undefined)
+
+  useEffect(() => {
+    onAuthStateChanged(setUser)
+  }, [])
+
+  const handleClick = () => {
+    loginWithGitHub().then(setUser).catch(err => {
+      console.log(err)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,10 +32,28 @@ export default function Home() {
       </Head>
 
       <main>
-      <h1 className={styles.title}>
-          <a href="https://nextjs.org">Devter</a>
-        </h1>
-        </main>
+        <section>
+        <img src='/devter-logo.png' alt='Logo' />
+          <h1>Devter</h1>
+          <h2>Talk about development<br />with developers 👩‍💻👨‍💻</h2>
+          {
+              user ?
+              <div>
+                <img src={user.avatar} />
+                <strong>{user.username}</strong>
+              </div> :
+                <Button onClick={handleClick}>
+                  <GitHub fill='#fff' width={24} height={24} />
+                  Login with GitHub
+                </Button>
+            }
+          {/* <Button>
+            <GitHub fill='white' width={32} height={32} />
+            Login with Github
+          </Button> */}
+        </section>    
+        
+      </main>
        
     </div>
   )
